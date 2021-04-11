f093ea2d1097c12147690fcd5da5eec42f6b9e04
Dominik Blum <dominikblum98@hotmail.com>
2021-04-11 17:41:32 +0200

    edit service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
+const Cache_name = "static_cache"
+const Static_assets = {
+    '/index.html'
+}
+
+async function preCache() {
+    const cache = await cache.open(Cache_name)
+    return cache.addAll(Static_assets)
+}
+
 self.addEventListener('install', event => {
     console.log("[sw] installed");
+    event.waitUntil(preCache())
 })
 self.addEventListener('activate', event => {
     console.log("[sw] actived");

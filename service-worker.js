28bd3d74b5f5e7f21296a007ac7a6dc2f0e04226
Dominik Blum <dominikblum98@hotmail.com>
2021-04-11 17:53:41 +0200

    repair

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,30 @@
 const Cache_name = "static_cache"
-const Static_assets = {
+const Static_assets = [
     '/index.html'
-}
+]
 
 async function preCache() {
-    const cache = await cache.open(Cache_name)
+    const cache = await caches.open(Cache_name)
     return cache.addAll(Static_assets)
 }
 
 self.addEventListener('install', event => {
-    console.log("[sw] installed");
+    console.log("installed");
     event.waitUntil(preCache())
 })
 self.addEventListener('activate', event => {
-    console.log("[sw] actived");
+    console.log("actived");
 })
+async function fetchAssets(event) {
+    try {
+        const response = await fetch(event.request)
+        return response
+    } catch (err) {
+        const cache = await caches.open(Cache_name)
+        return cache.match(event.request)
+    }
+}
 
 self.addEventListener('fetch', event => {
-    console.log("[sw] fetched");
+    console.log("fetched");
 })

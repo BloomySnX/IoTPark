1eeeab16269843bd548895e7602a22f73e3248db
Dominik Blum <dominikblum98@hotmail.com>
2021-04-18 15:36:39 +0200

    Add indexdb

--- script.js
+++ script.js
@@ -1,48 +1,109 @@
-var enterBtn = document.getElementById('enterBtn');
-var parkBtn = document.getElementById('parkBtn');
-var parkInput = document.getElementById('parkInput');
-var leaveBtn = document.getElementById('leaveBtn');
-var leaveInput = document.getElementById('leaveInput');
+class Parking {
+    constructor() {
+        this.enterBtn = document.getElementById('enterBtn');
+        this.parkBtn = document.getElementById('parkBtn');
+        this.parkInput = document.getElementById('parkInput');
+        this.leaveBtn = document.getElementById('leaveBtn');
+        this.leaveInput = document.getElementById('leaveInput');
+    }
+    init() {
+        this.initializeIndexedDb();
+        this.registerServiceWorker();
+    }
+    initializeIndexedDb() {
+        let messageLog = window.indexedDB.open('Parking');
 
-var logArray = [];
+        messageLog.onupgradeneeded = (event) => {
+            let db = event.target.result;
+            let logObjStore = db.createObjectStore('logObjStore', { autoIncrement: true });
 
-enterBtn.addEventListener('click', function() {
-    logAction('Wjazd', 0);
-});
-parkBtn.addEventListener('click', function() {
-    logAction('Parkowanie', parkInput);
-});
-leaveBtn.addEventListener('click', function() {
-    logAction('Opuszczanie', leaveInput);
-});
-
-function logAction(_action, _place) {
-    var date = new Date();
-
-    var logEvent = {
-        action: _action,
-        place: _place.value,
-        date: date
-    };
-
-    localStorage.setItem(_action, JSON.stringify(logEvent));
-    let storageObject = JSON.parse(localStorage.getItem(_action));
-    console.log(storageObject.action);
-    console.log(storageObject.place);
-    console.log(storageObject.date);
-
-
-    cookieStore.get('session_id')
-    cookieStore.set({ name: storageObject.action, value: storageObject.place });
-
-    cookieStore.addEventListener('change', (event) => {
-        for (const cookie of event.changed) {
-            if (cookie.name === 'session_id')
-                sessionCookieChanged(cookie.value);
+            logObjStore.createIndex('action', 'action', { unique: false });
+            logObjStore.createIndex('place', 'place', { unique: false });
+            logObjStore.createIndex('date', 'date', { unique: false });
         }
-        for (const cookie of event.deleted) {
-            if (cookie.name === 'session_id')
-                sessionCookieChanged(null);
+    }
+    logAction(_action, _place) {
+        return {
+            action: _action,
+            place: _place.value,
+            date: new Date()
+        };
+    }
+    formDataToDb(_action, _place) {
+        return new Promise((resolve, reject) => {
+            let messageLog = window.indexedDB.open('Parking');
+
+            messageLog.onsuccess = event => {
+                let objStore = messageLog.result.transaction('logObjStore', 'readwrite')
+                    .objectStore('logObjStore');
+                objStore.add(this.logAction(_action, _place));
+                resolve();
+            }
+
+            messageLog.onerror = err => {
+                reject(err);
+            }
+        });
+    }
+    formDataToServer() {
+        console.log(JSON.stringify(this.getFormData()));
+
+    }
+    registerServiceWorker() {
+        if (navigator.serviceWorker) {
+            navigator.serviceWorker.getRegistrations()
+                .then(registrations => {
+                    if (registrations.length === 0) {
+                        navigator.serviceWorker.register('service-worker.js');
+                    }
+                })
+                .then(() => {
+                    return navigator.serviceWorker.ready;
+                })
+                .then(registration => {
+                    this.newCarButton.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        this.formDataToDb("Wjazd", 0).then(function() {
+                            if (registration.sync) {
+                                registration.sync.register('message-to-log')
+                                    .catch(function(err) {
+                                        return err;
+                                    })
+                            }
+                        });
+                    })
+                    this.parkButton.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        this.formDataToDb("Parkowanie", parkInput).then(function() {
+                            if (registration.sync) {
+                                registration.sync.register('message-to-log')
+                                    .catch(function(err) {
+                                        return err;
+                                    })
+                            }
+                        });
+                    })
+                    this.leaveButton.addEventListener('click', (event) => {
+                        event.preventDefault();
+                        this.formDataToDb("Opuszczanie", leaveInput).then(function() {
+                            if (registration.sync) {
+                                registration.sync.register('message-to-log')
+                                    .catch(function(err) {
+                                        return err;
+                                    })
+                            }
+                        });
+                    })
+
+                })
+        } else {
+            this.button.addEventListener('click', () => {
+                this.formDataToServer();
+            });
         }
-    });
+    }
 }
+const coreJs = new Parking();
+window.addEventListener('load', () => {
+    coreJs.init();
+});

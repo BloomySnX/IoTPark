5b2d16dcbfa24e46fc28c787506d3b09e356177e
Dominik Blum <dominikblum98@hotmail.com>
2021-04-18 16:20:11 +0200

    add

--- script.js
+++ script.js
@@ -1,151 +1,56 @@
-class Parking {
-    constructor() {
-        this.enterBtn = document.getElementById('enterBtn');
-        this.parkBtn = document.getElementById('parkBtn');
-        this.parkInput = document.getElementById('parkInput');
-        this.leaveBtn = document.getElementById('leaveBtn');
-        this.leaveInput = document.getElementById('leaveInput');
-    }
+var enterBtn = document.getElementById('enterBtn');
+var parkBtn = document.getElementById('parkBtn');
+var parkInput = document.getElementById('parkInput');
+var leaveBtn = document.getElementById('leaveBtn');
+var leaveInput = document.getElementById('leaveInput');
 
-    init() {
-        this.initializeIndexedDb();
-        this.registerServiceWorker();
-    }
+var logArray = [];
 
-    initializeIndexedDb() {
-        let messageLog = window.indexedDB.open('Parking');
+enterBtn.addEventListener('click', function() {
+    logAction('Wjazd', 0);
+});
+parkBtn.addEventListener('click', function() {
+    logAction('Parkowanie', parkInput);
+});
+leaveBtn.addEventListener('click', function() {
+    logAction('Opuszczanie', leaveInput);
+});
 
-        messageLog.onupgradeneeded = (event) => {
-            let db = event.target.result;
-            let logObjStore = db.createObjectStore('logObjStore', { autoIncrement: true });
+function logAction(_action, _place) {
+    var date = new Date();
 
-            logObjStore.createIndex('action', 'action', { unique: false });
-            logObjStore.createIndex('place', 'place', { unique: false });
-            logObjStore.createIndex('date', 'date', { unique: false });
-        }
-    }
-    logAction(_action, _place) {
-        return {
-            action: _action,
-            place: _place.value,
-            date: new Date()
-        };
-    }
-    formDataToDb(_action, _place) {
-        return new Promise((resolve, reject) => {
-            let messageLog = window.indexedDB.open('Parking');
-
-            messageLog.onsuccess = event => {
-                let objStore = messageLog.result.transaction('logObjStore', 'readwrite')
-                    .objectStore('logObjStore');
-                objStore.add(this.logAction(_action, _place));
-                resolve();
-            }
-
-            messageLog.onerror = err => {
-                reject(err);
-            }
-        });
-    }
-    formDataToServer() {
-        console.log(JSON.stringify(this.getFormData()));
-        enterBtn.addEventListener('click', function() {
-            logAction('Wjazd', 0);
-        });
-        parkBtn.addEventListener('click', function() {
-            logAction('Parkowanie', parkInput);
-        });
-        leaveBtn.addEventListener('click', function() {
-            logAction('Opuszczanie', leaveInput);
-        });
-
-        function logAction(_action, _place) {
-            var date = new Date();
-
-            var logEvent = {
-                action: _action,
-                place: _place.value,
-                date: date
-            };
-
-            localStorage.setItem(_action, JSON.stringify(logEvent));
-            let storageObject = JSON.parse(localStorage.getItem(_action));
-            console.log(storageObject.action);
-            console.log(storageObject.place);
-            console.log(storageObject.date);
+    var exist = JSON.parse(localStorage.getItem(_action));
+    if (exist == null) exist = [];
 
 
-            cookieStore.get('session_id')
-            cookieStore.set({ name: storageObject.action, value: storageObject.place });
+    var logEvent = {
+        action: _action,
+        place: _place.value,
+        date: date
+    };
 
-            cookieStore.addEventListener('change', (event) => {
-                for (const cookie of event.changed) {
-                    if (cookie.name === 'session_id')
-                        sessionCookieChanged(cookie.value);
-                }
-                for (const cookie of event.deleted) {
-                    if (cookie.name === 'session_id')
-                        sessionCookieChanged(null);
-                }
-            });
-        }
 
-    }
-    registerServiceWorker() {
-        if (navigator.serviceWorker) {
-            navigator.serviceWorker.getRegistrations()
-                .then(registrations => {
-                    if (registrations.length === 0) {
-                        navigator.serviceWorker.register('service-worker.js');
-                    }
-                })
-                .then(() => {
-                    return navigator.serviceWorker.ready;
-                })
-                .then(registration => {
-                    this.enterBtn.addEventListener('click', (event) => {
-                        event.preventDefault();
-                        this.formDataToDb("Wjazd", 0).then(function() {
-                            if (registration.sync) {
-                                registration.sync.register('message-to-log')
-                                    .catch(function(err) {
-                                        return err;
-                                    })
-                            }
-                        });
-                    })
-                    this.parkBtn.addEventListener('click', (event) => {
-                        event.preventDefault();
-                        this.formDataToDb("Parkowanie", parkInput).then(function() {
-                            if (registration.sync) {
-                                registration.sync.register('message-to-log')
-                                    .catch(function(err) {
-                                        return err;
-                                    })
-                            }
-                        });
-                    })
-                    this.leaveBtn.addEventListener('click', (event) => {
-                        event.preventDefault();
-                        this.formDataToDb("Opuszczanie", leaveInput).then(function() {
-                            if (registration.sync) {
-                                registration.sync.register('message-to-log')
-                                    .catch(function(err) {
-                                        return err;
-                                    })
-                            }
-                        });
-                    })
 
-                })
-        } else {
-            this.button.addEventListener('click', () => {
-                this.formDataToServer();
-            });
-        }
-    }
+    localStorage.setItem("logEvent", JSON.stringify(logEvent));
+    exist.push(logEvent);
+    localStorage.setItem("allEvents", JSON.stringify(exist));
+
+
+
+
+
+    cookieStore.get('session_id')
+    cookieStore.set({ name: storageObject.action, value: storageObject.place });
+
 }
-const coreJs = new Parking();
-window.addEventListener('load', () => {
-    coreJs.init();
+
+cookieStore.addEventListener('change', (event) => {
+    for (const cookie of event.changed) {
+        if (cookie.name === 'session_id')
+            sessionCookieChanged(cookie.value);
+    }
+    for (const cookie of event.deleted) {
+        if (cookie.name === 'session_id')
+            sessionCookieChanged(null);
+    }
 });

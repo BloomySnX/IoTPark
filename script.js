86e68e9d889ac8e990e4f01dfe5938d2622e6b00
Dominik Blum <dominikblum98@hotmail.com>
2021-04-18 16:06:33 +0200

    add local storage

--- script.js
+++ script.js
@@ -49,6 +49,46 @@ class Parking {
     }
     formDataToServer() {
         console.log(JSON.stringify(this.getFormData()));
+        enterBtn.addEventListener('click', function() {
+            logAction('Wjazd', 0);
+        });
+        parkBtn.addEventListener('click', function() {
+            logAction('Parkowanie', parkInput);
+        });
+        leaveBtn.addEventListener('click', function() {
+            logAction('Opuszczanie', leaveInput);
+        });
+
+        function logAction(_action, _place) {
+            var date = new Date();
+
+            var logEvent = {
+                action: _action,
+                place: _place.value,
+                date: date
+            };
+
+            localStorage.setItem(_action, JSON.stringify(logEvent));
+            let storageObject = JSON.parse(localStorage.getItem(_action));
+            console.log(storageObject.action);
+            console.log(storageObject.place);
+            console.log(storageObject.date);
+
+
+            cookieStore.get('session_id')
+            cookieStore.set({ name: storageObject.action, value: storageObject.place });
+
+            cookieStore.addEventListener('change', (event) => {
+                for (const cookie of event.changed) {
+                    if (cookie.name === 'session_id')
+                        sessionCookieChanged(cookie.value);
+                }
+                for (const cookie of event.deleted) {
+                    if (cookie.name === 'session_id')
+                        sessionCookieChanged(null);
+                }
+            });
+        }
 
     }
     registerServiceWorker() {
